afd6a073d66349983bd6e8928bccde0363f3780c
MATSUMOTO, Katsuyoshi <github@katsyoshi.org>
2021-12-30 16:57:15 +0900

    implement for meckerel agent plugin

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use co2mon::{Result, Sensor};
+use std::time::{SystemTime, UNIX_EPOCH};
+
+fn main() -> Result<()> {
+    let sensor = Sensor::open_default()?;
+    let reading = sensor.read()?;
+    let time = match SystemTime::now().duration_since(UNIX_EPOCH) {
+        Ok(n) => n.as_secs(),
+        Err(_) => panic!("SystemTime before UNIX EPOCH!"),
+    };
+    let co2_metric_name = format!("{}.{}.{}", "CO2MINI", "co2", "living");
+    let temp_metric_name = format!("{}.{}.{}", "CO2MINI", "temp", "living");
+    println!("{}\t{}\t{}", co2_metric_name, reading.co2(), time.to_string());
+    println!("{}\t{}\t{}", temp_metric_name, reading.temperature(), time.to_string());
+    Ok(())
 }
